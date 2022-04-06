7cf9f09fe2c1f1dfcdc039fd23d3d9bb1b1d23ae
Hieu1050 <vuchihieu1050@gmail.com>
2022-04-06 12:45:46 +0900

    randomly picking an image for button event listener

--- react101-02-app/src/components/Meme.js
+++ react101-02-app/src/components/Meme.js
@@ -1,6 +1,12 @@
+
+import memesData from "../memesData"
 export default function Meme (){
     function handleClick (){
-        
+        let data_len = memesData.data.memes.length;
+        let random_image = memesData.data.memes[
+            Math.floor (Math.random () * data_len)
+        ];
+        console.log (random_image.url);
     }
 
     return (
