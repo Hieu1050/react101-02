c8a3c045d6ef5f2b490c3ef294371e40041b3ee8
Hieu1050 <vuchihieu1050@gmail.com>
2022-04-07 14:58:50 +0900

    react useState for rerendering values to UI

--- react101-02-app/src/components/Meme.js
+++ react101-02-app/src/components/Meme.js
@@ -1,17 +1,20 @@
 
+import { useState } from "react";
 import memesData from "../memesData"
 export default function Meme (){
+    const [imageUrl, setImageUrl] = useState("");
     function handleClick (){
-        let data_len = memesData.data.memes.length;
+        let data_len = memesData.data.memes.length
         let random_image = memesData.data.memes[
             Math.floor (Math.random () * data_len)
-        ];
-        console.log (random_image.url);
+        ]
+        console.log (random_image.url)
+        setImageUrl (random_image.url)
     }
 
     return (
         <main>
-            <form className="form">
+            <div className="form">
                 <input type = "text" 
                     className="form--input" 
                     placeholder="Top text" />
@@ -21,7 +24,8 @@ export default function Meme (){
                 <button onClick={handleClick} className="form--button"> 
                     Get a new image
                 </button>
-            </form>
+            </div>
+            <img src= {imageUrl} className="meme--image"/>
         </main>
     )
 }
